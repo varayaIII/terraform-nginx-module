dafc8add9238f8a2012e092dbfbc1a61a3ca5557
Víctor Araya A. <129128148+varayaIII@users.noreply.github.com>
2025-09-17 18:39:49 -0300

    Update main.tf

--- examples/basic_usage/main.tf
+++ examples/basic_usage/main.tf
@@ -1,1 +1,10 @@
+module "nginx_app" {
+  # La ruta apunta al módulo local
+  source = "../../../modules/nginx"
 
+  # Asignamos un nombre
+  app_name = "nginx-test-app"
+  
+  # Se usa solo una réplica para que la prueba sea más rápida
+  replicas = 1 
+}
