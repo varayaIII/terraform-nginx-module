4fe4f5d7c8702e456887f44280cbc9f2d3abdb80
Víctor Araya A. <129128148+varayaIII@users.noreply.github.com>
2025-10-08 18:30:20 -0300

    Update main.tf

--- modules/nginx/main.tf
+++ modules/nginx/main.tf
@@ -1,8 +1,4 @@
-provider "kubernetes" {
-  # Terraform asumirá la configuración de tu `kubeconfig` local
-  # que apunta a tu clúster (Minikube/Kind).
-}
-
+# Crea el Deployment de Nginx
 resource "kubernetes_deployment" "nginx" {
   metadata {
     name      = var.app_name
@@ -30,10 +26,40 @@ resource "kubernetes_deployment" "nginx" {
 
       spec {
         container {
-          image = var.app_image
           name  = var.app_name
+          image = var.app_image
+
           port {
             container_port = var.container_port
+          }
+
+          resources {
+            limits = {
+              cpu    = "200m"
+              memory = "128Mi"
+            }
+            requests = {
+              cpu    = "100m"
+              memory = "64Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = var.container_port
+            }
+            initial_delay_seconds = 5
+            period_seconds        = 10
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/"
+              port = var.container_port
+            }
+            initial_delay_seconds = 5
+            period_seconds        = 10
           }
         }
       }
@@ -41,19 +67,23 @@ resource "kubernetes_deployment" "nginx" {
   }
 }
 
-resource "kubernetes_service" "nginx" {
+# Crea el Service tipo LoadBalancer para exponer Nginx
+resource "kubernetes_service" "nginx_svc" {
   metadata {
-    name      = "${var.app_name}-service"
+    name      = "${var.app_name}-svc"
     namespace = var.namespace
   }
+
   spec {
     selector = {
-      app = kubernetes_deployment.nginx.metadata[0].labels.app
+      app = var.app_name
     }
+
     port {
       port        = 80
       target_port = var.container_port
     }
-    type = "NodePort" # Usamos NodePort para que sea fácil de probar localmente
+
+    type = "LoadBalancer"
   }
 }
