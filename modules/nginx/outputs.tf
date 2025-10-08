5e4956f34fdd8b52ec2b0187594a0e60e3bb35dc
Víctor Araya A. <129128148+varayaIII@users.noreply.github.com>
2025-10-08 18:30:40 -0300

    Update outputs.tf

--- modules/nginx/outputs.tf
+++ modules/nginx/outputs.tf
@@ -1,1 +1,15 @@
+output "deployment_name" {
+  description = "Nombre del deployment creado"
+  value       = kubernetes_deployment.nginx.metadata[0].name
+}
+
+output "service_name" {
+  description = "Nombre del servicio creado"
+  value       = kubernetes_service.nginx_svc.metadata[0].name
+}
+
+output "service_ip" {
+  description = "Dirección IP externa (LoadBalancer) del servicio"
+  value       = try(kubernetes_service.nginx_svc.status[0].load_balancer[0].ingress[0].hostname, null)
+}
 
